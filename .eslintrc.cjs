3ea6272ef4acc295fa28ac49992e12c3694a75b1
Brett Zamir <brettz9@yahoo.com>
2022-04-28 21:13:56 +0800

    feat: add main function

---
+++ .eslintrc.cjs
@@ -0,0 +1,14 @@
+'use strict';
+
+module.exports = {
+  extends: 'eslint:recommended',
+  env: {
+    es6: true,
+    node: true
+  },
+
+  parserOptions: {
+    sourceType: 'module',
+    ecmaVersion: 2022
+  }
+};
---
+++ CHANGES.md
@@ -0,0 +1,5 @@
+# CHANGES to `jsd2ts-assistant`
+
+## 0.1.0
+
+- Initial version
